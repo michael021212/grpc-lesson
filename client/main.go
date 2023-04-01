3b3b9e1f107809bd59df7290ce2edeeb07b5fa51
tanakashun <michael021212@gmail.com>
2023-04-01 16:21:17 +0900

    complete

---
+++ client/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc-lesson/pb"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func main() {
+	// SSL通信のための実装
+	certFile := "/Users/tanakashun/Library/Application Support/mkcert/rootCA.pem"
+	creds, err := credentials.NewClientTLSFromFile(certFile, "")
+	// サーバーとの接続を確立する
+	// WithInsecureは通信が暗号化されないので本番では使わない
+	// 代わりにWithTransportCredentialsを使う
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	// main関数の終了時に必ず接続をcloseする
+	defer conn.Close()
+
+	// fileServiceClient構造体を取得してclientに代入
+	// file_grpc.pb.goにあるfileServiceClientのListFilesメソッドを呼び出せるようにするため
+	client := pb.NewFileServiceClient(conn)
+
+	// callListFiles(client) // Unary RPC のケース
+	callDownload(client) // サーバーストリーミングのケース
+	// callUpload(client) // クライアントストリーミングのケース
+	// callUploadAndNotifyProgress(client) // 双方向ストリーミングのケース
+}
+
+// pb.FileServiceClient型のclientを引数に受け取り、
+func callListFiles(client pb.FileServiceClient) {
+	md := metadata.New(map[string]string{"authorization": "Bearer bad-token"})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	// file_grpc.pb.goに定義されているListFilesメソッド通りに実装する
+	// レシーバーはfileServiceClientのポインタとなっている
+	// ListFilesメソッドの第一引数には空のContextを渡し、
+	// 第二引数にfile.pb.goのListFilesRequest構造体を渡す
+	// ListFilesResponseが返ってくる
+	res, err := client.ListFiles(ctx, &pb.ListFilesRequest{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// filenamesのゲッターメソッドがfile.pb.goに生成されているのでそれを使う
+	fmt.Println(res.GetFilenames())
+}
+
+func callDownload(client pb.FileServiceClient) {
+	// タイムアウトの実装
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req := &pb.DownloadRequest{Filename: "name.txt"}
+	// FileService_DownloadClientインターフェースがstreamに入る
+	stream, err := client.Download(ctx, req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		// stream(FileService_DownloadClientインターフェース)のRecv()メソッドの返り値は*DownloadResponse
+		res, err := stream.Recv()
+		// サーバーからEOFが到達するとループを抜ける
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			resErr, ok := status.FromError(err) // grpcのエラーの場合はokがtrueになる
+			if ok {
+				if resErr.Code() == codes.NotFound {
+					log.Fatalf("Error Code: %v, Error Message: %v", resErr.Code(), resErr.Message())
+				} else if resErr.Code() == codes.DeadlineExceeded {
+					log.Fatalln("deadline exceeded")
+				} else {
+					log.Fatalln("unknown grpc error")
+				}
+			} else {
+				log.Fatalln(err)
+			}
+		}
+
+		log.Printf("Response from Donwload(bytes): %v", res.GetData())
+		log.Printf("Response from Donwload(string): %v", string(res.GetData()))
+	}
+}
+
+func callUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/Users/tanakashun/Desktop/udemy-grpc/grpc-lesson/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	// // FileService_UploadClientインターフェースがstreamに入る
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// アップロードするデータを入れる変数を作る
+	buf := make([]byte, 5)
+	for {
+		// データをbufに入れる。nにはサイズが入る
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// UploadRequestのオブジェクトを作って、streamに流す
+		req := &pb.UploadRequest{Data: buf[:n]}
+		sendErr := stream.Send(req)
+		if sendErr != nil {
+			log.Fatalln(sendErr)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	// リクエストの終了をサーバーに通知し、レスポンスを受け取る
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("recieved data size: %v", res.GetSize())
+}
+
+func callUploadAndNotifyProgress(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/Users/tanakashun/Desktop/udemy-grpc/grpc-lesson/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.UploadAndNotifyProgress(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// request
+	buf := make([]byte, 5)
+	go func() {
+		for {
+			n, err := file.Read(buf)
+			if n == 0 || err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			req := &pb.UploadAndNotifyProgressRequest{Data: buf[:n]}
+			sendErr := stream.Send(req)
+			if sendErr != nil {
+				log.Fatalln(sendErr)
+			}
+			time.Sleep(1 * time.Second)
+		}
+
+		err := stream.CloseSend()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// response
+	ch := make(chan struct{})
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			log.Printf("recieved message: %v", res.GetMsg())
+		}
+		close(ch)
+	}()
+	<-ch
+}
---
+++ server/main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"grpc-lesson/pb"
+	"log"
+	"net"
+	"os"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware" // 'go get github.com/grpc-ecosystem/go-grpc-middleware'してから手動追加
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"  // 'go get github.com/grpc-ecosystem/go-grpc-middleware'してから手動追加
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+)
+
+// file_grpc.pb.goに定義されているUnimplementedFileServiceServer構造体を指定する
+// server の実体はUnimplementedFileServiceServer ということ
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+// serverをレシーバとする?ListFiles()メソッドを定義
+// storage/配下のファイル名を返す
+func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
+	fmt.Println("ListFiles was invoked")
+
+	dir := "/Users/tanakashun/Desktop/udemy-grpc/grpc-lesson/storage"
+
+	paths, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+	for _, path := range paths {
+		if !path.IsDir() {
+			filenames = append(filenames, path.Name())
+		}
+	}
+	// filenames => [name.txt sports.txt]
+
+	// file.pb.goのListFilesResponse構造体のFilenamesフィールドにファイル名を入れたインスタンスを受け取る
+	res := &pb.ListFilesResponse{
+		Filenames: filenames,
+	}
+	return res, nil
+	// res => "filenames:"name.txt"  filenames:"sports.txt""
+	// client側ではres.GetFilenames()を使って[name.txt sports.txt]という出力にする
+}
+
+// pb.DownloadRequest構造体のインスタンスと、pb.FileService_DownloadServerストリームを引数とするメソッド
+func (*server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadServer) error {
+	fmt.Println("Download was invoked")
+
+	// .protoファイルにより生成されたGetFilenameメソッドを使う
+	filename := req.GetFilename()
+	path := "/Users/tanakashun/Desktop/udemy-grpc/grpc-lesson/storage/" + filename
+
+	// ファイルの存在チェック
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return status.Error(codes.NotFound, "file was not found")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 長さが5のバイト型スライスを用意
+	buf := make([]byte, 5)
+	for {
+		// Read は、データを与えられたバイトスライスへ入れ、入れたバイトのサイズとエラーの値を返します。
+		// ストリームの終端は、 io.EOF のエラーで返します。
+		// nには毎回5が入り、最後のループだけ3とかになる可能性がある
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		// 送信するデータがもうない場合はio.EOFがerrに入っており、return err が実行される
+		if err != nil {
+			return err
+		}
+
+		// bufにデータが読み込まれた場合、ストリームを介してクライアントにデータを転送する
+		// Dataフィールドにbuf[:n]を入れて新しいpb.DownloadResponseオブジェクトを作成している
+		// bufはインデックスが0~4までの5個の要素なので、buf[:n]のnが5の場合、n[:0]~n[:4]までの全てを指定したことになる
+		res := &pb.DownloadResponse{Data: buf[:n]}
+		sendErr := stream.Send(res)
+		if sendErr != nil {
+			return sendErr
+		}
+		// 今回は一瞬で処理が終わらないようにする
+		time.Sleep(1 * time.Second)
+	}
+
+	// return nil をすると、ストリームはエンドオブファイルを返してレスポンスが終了する
+	// Goではnilを返すことで、エラーが発生しなかったことを明示的に示す慣習がある
+	return nil
+}
+
+func (*server) Upload(stream pb.FileService_UploadServer) error {
+	fmt.Println("Upload was invoked")
+
+	// クライアントからアップロードされたデータを格納するためのバッファーを用意
+	var buf bytes.Buffer
+	for {
+		req, err := stream.Recv()
+		// クライアントからの送信が終了した場合、サイズを返す
+		if err == io.EOF {
+			res := &pb.UploadResponse{Size: int32(buf.Len())}
+			return stream.SendAndClose(res)
+		}
+		if err != nil {
+			return err
+		}
+
+		data := req.GetData()
+		log.Printf("recieved data(bytes): %v", data)
+		log.Printf("recieved data(string): %v", string(data))
+		buf.Write(data)
+	}
+}
+
+func (*server) UploadAndNotifyProgress(stream pb.FileService_UploadAndNotifyProgressServer) error {
+	fmt.Println("UploadAndNotifyProgress was invoked")
+
+	size := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		data := req.GetData()
+		log.Printf("recieved data: %v", data)
+		size += len(data)
+
+		res := &pb.UploadAndNotifyProgressResponse{
+			Msg: fmt.Sprintf("recieved %vbytes", size),
+		}
+		err = stream.Send(res)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+// gRPCが提供する決まった形があり、それを満たす関数myLoggingというインターセプターを作る
+// これをgrpc.NewServer()にオプションとして渡せばこのインターセプターを追加できる
+func myLogging() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		log.Printf("request data: %+v", req)
+
+		// handlerはクライアントからコールされるメソッド。この前後にlogを書くことで
+		// "ListFiles was invoked"という出力の前後にログ出力ができるようになる
+		resp, err = handler(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		log.Printf("response data: %+v", resp)
+
+		return resp, nil
+	}
+}
+
+// 認証のインターセプターを実装する
+func authorize(ctx context.Context) (context.Context, error) {
+	// クライアントから送られてきたメタデータからBearerというキーで認証情報を取り出す
+	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	if err != nil {
+		return nil, err
+	}
+
+	if token != "test-token" {
+		return nil, status.Error(codes.Unauthenticated, "token is invalid") // grpcのエラーコードを使う
+	}
+	return ctx, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// SSL通信のため、自前の証明書と秘密鍵を設定
+	creds, err := credentials.NewServerTLSFromFile(
+		"ssl/localhost.pem",
+		"ssl/localhost-key.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// grpcライブラリで定義されているNewServer関数を変数sに代入
+	// NewServer()は、grpcライブラリで定義されているServer構造体のポインタを返す
+	s := grpc.NewServer(
+		grpc.Creds(creds),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				myLogging(),
+				grpc_auth.UnaryServerInterceptor(authorize)))) // 引数は応用編で追記。複数のインターセプターを書くにはChainUnaryServerを使う
+	// file_grpc.pb.goに定義されているRegisterFileServiceServer関数を使う
+	// 第一引数にgrpcサーバー、
+	// 第二引数に今回はFileServiceServerインターフェースを実装した構造体:server(上部で定義)を渡す
+	// grpcサーバーに構造体の内容を登録する
+	// つまり、grpcサーバーが、ListFilesなどのメソッドを提供できるようになる
+	pb.RegisterFileServiceServer(s, &server{})
+
+	fmt.Println("server is runnning...")
+	// .Serve(lis)で、lis(localhost:50051)でサーバーを起動
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
